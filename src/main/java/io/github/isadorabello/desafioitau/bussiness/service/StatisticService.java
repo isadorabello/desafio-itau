dcf0d20cb7f3cee845e7fcdbd2d0130e74c45417
isadorabello <isadora.bello@gec.inatel.br>
2025-03-20 18:18:43 -0300

    feat: creating the statistics dto controller

---
+++ src/main/java/io/github/isadorabello/desafioitau/bussiness/service/StatisticService.java
@@ -0,0 +1,24 @@
+package io.github.isadorabello.desafioitau.bussiness.service;
+
+
+import io.github.isadorabello.desafioitau.controller.dtos.StatisticResponseDTO;
+import io.github.isadorabello.desafioitau.controller.dtos.TransactionRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticService {
+
+    public final TransactionService tService;
+
+    public StatisticResponseDTO transactionsStats (Integer intervalo){
+        List<TransactionRequestDTO> transactions = tService.searchStatiscs(intervalo);
+        DoubleSummaryStatistics stats = transactions.stream().mapToDouble(TransactionRequestDTO::valor).summaryStatistics();
+        return new StatisticResponseDTO(stats.getCount(), stats.getSum(), stats.getAverage(), stats.getMin(), stats.getMax());
+    }
+
+}
---
+++ src/main/java/io/github/isadorabello/desafioitau/controller/dtos/StatisticResponseDTO.java
@@ -0,0 +1,4 @@
+package io.github.isadorabello.desafioitau.controller.dtos;
+
+public record StatisticResponseDTO(Long count, Double sum, Double avg, Double min, Double max) {
+}
