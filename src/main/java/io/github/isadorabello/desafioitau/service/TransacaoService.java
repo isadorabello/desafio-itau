95c1e8797c1caea7dde87fdfe21f5a8e3c718c73
isadorabello <isadora.bello@gec.inatel.br>
2025-03-20 10:21:19 -0300

    feat: adjusting service class

--- src/main/java/io/github/isadorabello/desafioitau/service/TransacaoService.java
+++ src/main/java/io/github/isadorabello/desafioitau/service/TransacaoService.java
@@ -3,12 +3,26 @@ package io.github.isadorabello.desafioitau.service;
 import io.github.isadorabello.desafioitau.model.Transacao;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Service
 public class TransacaoService {
     // não pode usar banco de dados -> usar memoria da aplicação -> usar uma collection (threadSafe) -> concurrent linked queue
-
     private final Queue<Transacao> transacoes = new ConcurrentLinkedQueue<>();
+
+    public void adicionarTransacao( Transacao t){
+        transacoes.add(t);
+    }
+
+    public void limparTransacao(){
+        transacoes.clear();
+    }
+
+    public DoubleSummaryStatistics getEstatisticas(){
+        OffsetDateTime agora = OffsetDateTime.now();
+        return transacoes.stream().filter(t -> t.getDataHora().isAfter(agora.minusSeconds(60))).mapToDouble(Transacao::getValor).summaryStatistics();
+    }
 }
